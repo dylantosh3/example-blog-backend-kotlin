125afba7210a10cc69f5ac9ef01a7d5710c7be64
Dylan Tosh <dylantosh@yahoo.com>
2020-06-15 14:40:28 -0500

    Readded Okta Dependency Added SecurityConfiguration.kt Altered application.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.okta.spring:okta-spring-boot-starter:1.4.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
