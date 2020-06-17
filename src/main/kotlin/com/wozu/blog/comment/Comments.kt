19f39fd5908e8bc503b85dc82ad310a5f2be3062
Dylan Tosh <dylantosh@yahoo.com>
2020-06-17 14:32:19 -0500

    Removed @Requestbody from delete methods Removed variables from various classes

--- src/main/kotlin/com/wozu/blog/comment/Comments.kt
+++ src/main/kotlin/com/wozu/blog/comment/Comments.kt
@@ -1,20 +1,15 @@
 package com.wozu.blog.comment
 
-import com.wozu.blog.article.Articles
-import com.wozu.blog.user.Users
 import javax.persistence.*
-import java.time.OffsetDateTime
 
 @Entity
 @Table(name="comments")
 data class Comments(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        var commentsid: Long = 0,
+        var id: Long = 0,
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var articlesid: Long = 0,
         var commenter: String = "",
-        var body: String = "",
-        //var createdAt: OffsetDateTime = OffsetDateTime.now(),
-        var updatedAt: OffsetDateTime = OffsetDateTime.now()
+        var body: String = ""
 )
--- src/main/kotlin/com/wozu/blog/user/UserRepository.kt
+++ src/main/kotlin/com/wozu/blog/user/UserRepository.kt
@@ -2,5 +2,4 @@ package com.wozu.blog.user
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<Users, Long> {
-}
+interface UserRepository : JpaRepository<Users, Long>
